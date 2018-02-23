173f4295e230a5601bbad750b221d4b4084490d0
mikupaccho <nozaki@scouty.co.jp>
2018-02-23 18:46:01 +0900

    完成2

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -1,52 +1,48 @@
-# -*- coding: utf-8 -*-
 import random
-from slackbot.bot import respond_to     # @botname: で反応するデコーダ
-from slackbot.bot import listen_to      # チャネル内発言で反応するデコーダ
-from slackbot.bot import default_reply  # 該当する応答がない場合に反応するデコーダ
+from slackbot.bot import listen_to, respond_to
 
-STATUS = {
-    'is_attendance': False,
-    'attendance_list': []
+g_status = {
+    'is_open': False,
+    'attendee_list': [],
 }
 
-LIMIT_MEMBER_COUNT = 6
-
-
-
-@respond_to('メンション')
-def mention_func(message):
-    message.reply('私にメンションと言ってどうするのだ') # メンション
-    send_user = message.channel._client.users[message.body['user']][u'name']
-    print(send_user)
-    message_text=message.body['text']
-    print(message_text)
+LIMIT_MEMBER_COUNT = 7
 
 
 @listen_to('.+')
 def listen_func(message):
-    send_user = message.channel._client.users[message.body['user']][u'name']
-    message_text=message.body['text']
-    if STATUS['is_attendance']:
-        print(send_user,message_text)
-        if message_text in ['はい']:
-            STATUS['attendance_list'].append(send_user)
+    send_user = message.channel._client.users[message.body['user']]['name']
+    message_text = message.body['text']
+    if g_status['is_open']:
+        print(send_user, message_text)
+        if message_text in ['はい', '行きます', 'おなかすいた']:
+            g_status['attendee_list'].append('<@{}>'.format(send_user))
+
 
 @respond_to('募集')
 def start(message):
     message.send('募集を開始します')
-    STATUS['is_attendance'] = True
+    g_status['is_open'] = True
+
 
 @respond_to('終了')
-def start(message):
+def end(message):
     message.send('募集を終了します')
-    STATUS['is_attendance'] = False
-    print(STATUS['attendance_list'])
-    attendance_list = list(set(STATUS['attendance_list']))
-    if len(attendance_list) >= LIMIT_MEMBER_COUNT:
-        random.shuffle(attendance_list)
-        member_count = len(attendance_list)//2
-        message.send(','.join(attendance_list[:member_count]))
-        message.send(','.join(attendance_list[member_count:]))
+    g_status['is_open'] = False
+    print(g_status['attendee_list'])
+    attendee_list = list(set(g_status['attendee_list']))
+    if len(attendee_list) >= LIMIT_MEMBER_COUNT:
+        random.shuffle(attendee_list)
+        member_count = len(attendee_list)//2
+        message.send('*チームガスパッチョ*')
+        for name in attendee_list[:member_count]:
+            message.send(name)
+        message.send('*チームデンパッチョ*')
+        for name in attendee_list[member_count:]:
+            message.send(name)
+        
     else:
-        message.send(','.join(attendance_list))
-    STATUS['attendance_list'] = []
+        message.send('*チームガスパッチョ*')
+        for name in attendee_list:
+            message.send(name)
+    g_status['attendee_list'] = []
